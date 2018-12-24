e609594aa6ee8c197516b931d0abc4977ef2a992
qweabc <480674076@qq.com>
2018-12-24 21:52:34 +0800

    更新第669题

---
+++ leetcode/669. Trim a Binary Search Tree.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} L
+ * @param {number} R
+ * @return {TreeNode}
+ */
+var trimBST = function(root, L, R) {
+    if(!root)
+        return null;
+    let val = root.val;
+    if(val < L)
+        return root.right ? trimBST(root.right,L,R) : null;
+    else if(val > R)
+        return root.left ? trimBST(root.left,L,R) : null;
+    else{
+        if(val === L){
+            root.left = null;
+            root.right = trimBST(root.right,L,R);
+        } else if(val === R){
+            root.right = null;
+            root.left = trimBST(root.left,L,R);
+        } else{
+            root.right = trimBST(root.right,L,R);
+            root.left = trimBST(root.left,L,R);
+        }
+    }
+    return root;
+};
